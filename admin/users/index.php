bfed3c4ba5a0887da5e48242a841489bf41ea6a3
shin <minara73@gmail.com>
2026-06-11 17:16:06 +0900

    Revert "Remove duplicate admin users footer"
    This reverts commit e2a4e5f2d734ea0eff0e5964025544eefa39dae5.

--- admin/users/index.php
+++ admin/users/index.php
@@ -197,3 +197,8 @@ require SMARTCMS_ROOT . '/admin/head.php';
 $SMARTCMS_FOOT = [];
 require SMARTCMS_ROOT . '/admin/foot.php';
 ?>
+
+<?php
+$SMARTCMS_FOOT = [];
+require SMARTCMS_ROOT . '/admin/foot.php';
+?>
